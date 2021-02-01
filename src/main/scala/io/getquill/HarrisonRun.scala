47b5c46672b018d91c1eb3c54971f699e837e2c8
Harrison <hsl39@drexel.edu>
2021-02-01 12:48:32 -0500

    Test

--- src/main/scala/io/getquill/HarrisonRun.scala
+++ src/main/scala/io/getquill/HarrisonRun.scala
@@ -9,12 +9,23 @@ object HarrisonRun {
     /*
     val x : Int = 6
     val y : Int = 5
-    */
+  
 
     val x : String = "h"
     val y : String = "o"
 
-    val output = quote{ x.toLowerCase }//compile
+    val output = quote{ x.toUpperCase }//compileee
     println(output)
+    */
+
+    /*
+    */
+    case class Age(value: Int) extends Embedded  
+    case class Person(name: String, age: Age)
+
+    inline def q = quote{ query[Person].insert(Person("Joe", Age(123))) }
+    println(q.ast)
+    
+
   }
 }
